ebaa9906eac49455020ab428a7b69285b40a577b
rwangari <rwangari@rwanda.cmu.edu>
2018-07-03 23:41:26 +0200

    adding a page to load data

---
+++ src/LoadingData.java
@@ -0,0 +1,13 @@
+
+public class LoadingData {
+
+	public LoadingData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
